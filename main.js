2891d78dd78aa2610a1bba94d0ab026b55c92129
filomenoPonce580 <110742301+filomenoPonce580@users.noreply.github.com>
2023-02-02 09:15:19 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+//one way. split into numbers and join individual numbers?
+function sumOfIntegersInString(s){
+  const searchNumber = s.split(/[^0-9]/gi)
+  let total = 0
+  for (let i = 0; i < searchNumber.length; i++) {
+    total += Number(searchNumber[i])
+  }
+  return total
+}
 
+//
+function sumOfIntegersInString(s){
+  return s.replace(/[^0-9]/g, ' ').split(' ').reduce((a,b) => a + +b,0)
+}
